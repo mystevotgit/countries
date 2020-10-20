669db3f2394c9b5df02997d5e13de0d69e37b0e8
mystevotgit <oyeolatunji@gmail.com>
2020-10-20 01:38:40 +0100

    create topbar and search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Topbar from './components/Topbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Topbar />
+      <div className="appbody">
+        <div className="main" >
+          <div className="query-elements" >
+            <form className="form" >
+              <input type="text" placeholder="Search for a country" id="search" />
+            </form>
+          </div>
+          <div className="grid">
+            <div className="model" >
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Topbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Topbar() {
+    return (
+        <div className="top-component">
+            <h3 className="where" >Where in the world?</h3>
+            <h4 className="dark" >Dark Mode</h4>
+        </div>
+    )
+}
+
+export default Topbar
